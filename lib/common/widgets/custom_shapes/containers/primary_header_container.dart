47b7d1e741839e144fec86db1dddf838f016c25d
Tharmapalan Nevaashahan <79234817+Nevaashahan@users.noreply.github.com>
2024-04-06 06:48:18 +0530

    Custom_shapes_using_clip_path

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,66 @@
+
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary1,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: -150,
+                  right: -250,
+                  child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1),
+                  )),
+              Positioned(
+                  top: 100,
+                  right: -300,
+                  child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1),
+                  )),
+              Positioned(
+                  top: 200,
+                  left: -300,
+                  child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TCurvedEdgeWidget extends StatelessWidget {
+  const TCurvedEdgeWidget({
+    super.key,
+    this.child,
+  });
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCustomCurvedEdges(),
+      child: child,
+    );
+  }
+}
--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +9,16 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Container()
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
